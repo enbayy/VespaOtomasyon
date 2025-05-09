d25ff09cb2e192d1ea46b1ab878f434bde72ca30
Enes Bayar <103318928+enbayy@users.noreply.github.com>
2025-05-09 18:13:23 +0300

    edited

--- src/pages/CookieBanner.jsx
+++ src/pages/CookieBanner.jsx
@@ -32,7 +32,7 @@ const CookieBanner = () => {
             </p>
             <div className="flex space-x-4 mt-2">
                 <button
-                    className="bg-[#08467e] hover:bg-[#1d3b55] text-white px-6 py-2 text-base font-semibold rounded"
+                    className="bg-[#af1816] hover:bg-[#1d3b55] text-white px-6 py-2 text-base font-semibold rounded"
                     onClick={handleAccept}
                 >
                     Kabul Et
